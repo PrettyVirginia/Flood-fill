d14956751c3ccb664f3c99fa72ea034ca92c49dd
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-03-21 16:12:16 +0000

    New translations en.json (Lithuanian)

--- examples/test-SO3.py
+++ examples/test-SO3.py
@@ -125,4 +125,7 @@ for t in linspace(0, lietraj2.duration, 1000):
     robot.SetTransform(M)
     isincollision = (env.CheckCollision(robot, CollisionReport()))
     if (isincollision):
-        print "in collision", " ", t, "/
+        print "in collision", " ", t, "/" , lietraj2.duration
+    time.sleep(0.01)
+
+# lietraj2.P
