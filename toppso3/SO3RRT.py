700c8891dce3ae89957c5090fd27e830686761d5
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-05-15 14:11:18 +0000

    jdoc

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -1,3 +1,59 @@
 
 #RRT implementation for reorientation with collision-free
 from openravepy import *
+from pylab import *
+
+import time
+import string
+import numpy as np
+import copy
+import random
+import os
+
+import lie
+
+import Utils
+import Heap
+
+import TOPP
+from TOPP import TOPPpy
+from TOPP import Trajectory
+
+
+
+# global variables for RRTPlanners
+FW = 0
+BW = 1
+REACHED = 0
+ADVANCED = 1
+TRAPPED = 2
+
+# gobal variables for collision checking 
+INCOLLISION = -1
+OK = 1
+
+# def vectfromquat
+# def eulerfromquat
+# def quatfromuler
+# def quatfromvect
+
+class Config():
+    """Attributes:
+         q   -- quaternion vector
+         qs  -- angular velocity
+    """
+    def __init__(self, q, qs = None, qss = None):
+        self.q = q
+        if (qs == None):
+            self.qs = zeros(3)
+        else:
+            self.qs = qs
+
+
+class Vertex():
+    """Attributes:
+         config     -- stores a Config obj
+         parent     -- the parent for FW vertex, the child for BW vertex
+         trajstring -- a trajectory from its parent (or child)
+         # sdmin      -- minimum reachable sd
+         # sdmax      -- maximum reachable sd
