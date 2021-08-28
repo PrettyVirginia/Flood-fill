8924bf093cc14f701e66be6665e7e029a6fbde08
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-08-28 16:04:46 +0000

    f2

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -111,4 +111,6 @@ def ComputeSE3Constraints(SE3traj, taumax, fmax, discrtimestep, I = None, m = No
         snr = sin(nr)
         cnr = cos(nr)
         rcrd = cross(r,rd)
-        rdrd 
+        rdrd = dot(r,rd)
+
+        Amat =  eye(3) - (1-cnr)/nr2*R + (nr-sn
