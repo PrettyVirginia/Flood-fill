53a8f03ed4ece9cbb5bcc30b7ed41d3f19f665dc
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-10-02 21:31:15 +0000

    Fix UI issues

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -152,4 +152,4 @@ def ComputeSE3Constraints(SE3traj, taumax, fmax, discrtimestep, I = None, m = No
 ######################## se3 traj collision checking ########################
 
 def CheckCollisionSE3Traj( robot, transtraj, rtraj, R_beg,  checkcollisiontimestep = 1e-3):
-    """CheckCollisionSE3Traj accep
+    """CheckCollisionSE3Traj accepts a robot and trans, rot trajectory object as its inp
