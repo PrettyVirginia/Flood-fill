37eeea4409c14ab7800115c185c4804a9aeade50
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-11-01 10:04:04 +0000

    README angepasst

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -180,4 +180,6 @@ def CheckCollisionSE3Traj( robot, transtraj, rtraj, R_beg,  checkcollisiontimest
 def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expectedduration = -1,  meanduration = 0, upperlimit = -1, plotdura = None):
     if plotdura == 1:
         plt.axis([0, maxiter, 0, se3traj.duration])
-       
+        plt.ion()
+        plt.show()
+        ylabel('
