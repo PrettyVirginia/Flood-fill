18a72d89e2dc42b9da632d8eff8502d6d1f1f94a
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2021-12-30 21:19:25 +0000

    Updated Galician translations

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -255,4 +255,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
         R_beg = lietraj.EvalRotation(t0)
         R_end = lietraj.EvalRotation(t1)
         omega0 = lietraj.EvalOmega(t0)
-        omega1 = 
+        omega1 = lietraj.EvalOmega(t1)
+        shortcutrtraj = l
