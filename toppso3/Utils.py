4a591039eefad967d91a7455770a79d58ea077d1
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-01-09 07:29:38 +0000

    release 0.3.3

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -263,4 +263,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
         v_beg = transtraj.Evald(t0)
         v_end = transtraj.Evald(t1)
         
-        shortcuttranstraj = Trajectory.PiecewisePolynomialTrajectory.FromString(TrajString3rdDeg
+        shortcuttranstraj = Trajectory.PiecewisePolynomialTrajectory.FromString(TrajString3rdDegree(t_beg,t_end,v_beg,v_end, T))
+        
