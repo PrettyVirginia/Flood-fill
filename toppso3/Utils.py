afbf5963e9fc423818e566bbde96cca92a384b19
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-02-26 21:30:14 +0000

    Bump version to 0.2.2

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -295,4 +295,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
                     rtraj = newrtraj
                     transtraj = newtranstraj
                     lietraj = newlietraj
-   
+                    se3traj = newse3traj
+                    
+  
