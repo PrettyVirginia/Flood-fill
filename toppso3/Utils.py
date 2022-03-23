e8ea3264ca1b34e4584cf39311c5a8ef9accb995
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-03-23 07:52:32 +0000

    Remove deprecated files

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -324,4 +324,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
 
 
 
-##########################
+#############################
+
+def ReplaceTransTrajectorySegment(originaltrans
