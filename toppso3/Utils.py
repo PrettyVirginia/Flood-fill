df948e96ef3af6957f51cde9687c83f0cd70fe0a
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-04-20 09:38:51 +0000

    messages

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -365,4 +365,4 @@ def ReplaceTransTrajectorySegment(originaltranstraj, transtrajsegment, t0,t1):
         ## b is a poly1d object
         b = b*a.coeffs[0] ## multiply back by a_n *** this multiplication does not commute
         
-        newpoly = Trajectory.Polyno
+        newpoly = Trajectory.Polynomial(b.coeffs.tolist()[::-1]) ## TOPP co
