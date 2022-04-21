425d282944759411854da85c3c3cc0bba9083860
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-04-21 13:57:37 +0000

    chore(package): update eslint to version 4.7.2

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -366,4 +366,5 @@ def ReplaceTransTrajectorySegment(originaltranstraj, transtrajsegment, t0,t1):
         b = b*a.coeffs[0] ## multiply back by a_n *** this multiplication does not commute
         
         newpoly = Trajectory.Polynomial(b.coeffs.tolist()[::-1]) ## TOPP convention is weak-term-first
-        newpoly_list.ap
+        newpoly_list.append(newpoly)
+    remchunk1 = Trajectory.Chu
