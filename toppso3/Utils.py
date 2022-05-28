fc9d66fe51acd4488dcf1431fb7294631e7282d4
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-05-28 11:24:13 +0000

    tiny bug fix

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -394,4 +394,5 @@ def CheckCollisionTraj(robot, trajectory, R_beg, checkcollisiontimestep = 1e-3):
             transformation = eye(4)
             transformation[0:3,0:3] = lie.EvalRotation(R_beg, traj, s)
             robot.SetTransform(transformation)           
-            isincollision = (env.
+            isincollision = (env.CheckCollision(robot, CollisionReport()))
+            #print 
