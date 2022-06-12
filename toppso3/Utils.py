18be15413e3c267e9be48f3f4cbd6c05e1ebf506
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-06-12 06:34:06 +0000

    Multiple improvements

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -409,4 +409,4 @@ def CheckCollisionTraj(robot, trajectory, R_beg, checkcollisiontimestep = 1e-3):
 
 
 ############################# SHORTCUTING SO3 ############################
-def Shortcut(robot, taumax, vmax, lietraj,  m
+def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  meanduration = 0, upper
