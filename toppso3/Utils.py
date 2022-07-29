b42c21d1d40d8c1a110c2c2b2cfa9f03e76f9aa6
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-07-29 14:19:35 +0000

    Another example

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -479,4 +479,6 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
             if t1 > upperlimit:
                 t1 = upperlimit
                 if (t1 < t0):
-                    temp
+                    temp = t0
+                    t0 = t1
+                    t1 = te
