506a735e04fb6067f15b3b4dc32734cddce29682
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-09-03 08:25:30 +0000

    Add missing argument

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -533,4 +533,4 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
 
             # print "T:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt
         
-    print "\03
+    print "\033[1;32mT:", nnotretimable, "; S:", nnotshorter , "
