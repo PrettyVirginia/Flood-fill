f9b0d5a9c9eb7a792d7798a286ea6ef190eabadc
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-09-04 07:29:00 +0000

    Fix tags

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -534,4 +534,4 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
             # print "T:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt
         
     print "\033[1;32mT:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt, "\033[0m"
-    print
+    print "\033[1;32m", originalduration - lietraj.dur
