f8707b0d31f82ffba128099a973a4c8c8271a343
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-09-18 06:29:56 +0000

    72

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -563,4 +563,5 @@ def ReplaceTrajectorySegment(originallietraj, trajsegment, t0, t1):
     ic0, remc0 = originallietraj.trajlist[i0].FindChunkIndex(rem0)
     # print "c0", originallietraj.trajlist[i0].FindChunkIndex(rem0) ##
           # check if rem0 falls in the first chunk, if not, ...
-  
+    if ic0 > 0:
+        for c in originallietraj.trajlist[i
