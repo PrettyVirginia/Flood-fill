23ffdaeb2abdc162466db2bfb6d034c30053fb34
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-10-07 12:15:10 +0000

    [maven-release-plugin] prepare release 0.1.3

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -597,4 +597,4 @@ def ReplaceTrajectorySegment(originallietraj, trajsegment, t0, t1):
             r[i] = r[i] - remc1
         b = np.poly1d(r, True) ## reconstruct a new polynomial from roots
         ## b is a poly1d object
-        b = b*a.coeffs[0] ## multiply back 
+        b = b*a.coeffs[0] ## multiply back by a_n *** this multiplication does not commute
