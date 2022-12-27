2c7633f9c9bbc83b30af8176d9769520f8d4eed6
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2022-12-27 14:57:55 +0000

    minor version update

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -736,4 +736,4 @@ def ReadSE3TrajFiles(rlistfilename, se3trajfilename):
     ## Read rlist
     with open(rlistfilename, 'r') as file:
         data_rliststring = file.read()
- 
+    list = [float(x) for x in data_rlist
