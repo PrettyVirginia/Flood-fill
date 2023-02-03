e7ee8870d0f0ec0c7ac70d5975ffa2dfa9932b6d
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2023-02-03 12:28:17 +0000

    shenanigans-manager@0.2.20

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -775,3 +775,4 @@ def PlotSE3(se3traj, rlist,  dt = 0.01, figstart=0,vmax=[],accelmax=[],taumax=[]
     transtraj, rottraj = TransRotTrajFromSE3Traj(se3traj)
     lietraj = lie.SplitTraj2(rlist, rottraj)
     
+    lietraj.Plot(dt,figstart,vmax[:3],accelmax,tau
