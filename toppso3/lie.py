53f9b5f3478aff98a4f021deb581d12723f385b0
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2023-03-02 15:32:58 +0000

    :art: Update files

--- toppso3/lie.py
+++ toppso3/lie.py
@@ -60,3 +60,58 @@ class LieTraj():
         return dot(Bmat(r),rdd) + dot(rd,tensordot(Ctensor(r),rd,([2],[0])))
 
     # Torques
+    def EvalTorques(self,s,I):
+        i, remainder = self.FindTrajIndex(s)
+        r = self.trajlist[i].Eval(remainder)
+        rd = self.trajlist[i].Evald(remainder)
+        rdd = self.trajlist[i].Evaldd(remainder)
+        omega = dot(Amat(r),rd)
+        alpha =  dot(Bmat(r),rdd) + dot(rd,tensordot(Ctensor(r),rd,([2],[0])))
+        return dot(I,alpha) + cross(omega,dot(I,omega))
+
+    
+    def Plot(self,dt=0.01,figstart=0,vmax=[],accelmax=[],taumax=[],I=None):
+
+        tvect = arange(0, self.duration + dt, dt)
+        omegavect = array([self.EvalOmega(t) for t in tvect])
+        figure(figstart)
+        clf()
+        
+        plt.plot(tvect,omegavect[:,0],'--',label = '$\omega^1$',linewidth = 2)
+        plt.plot(tvect,omegavect[:,1],'-.',label = '$\omega^2$',linewidth = 2)
+        plt.plot(tvect,omegavect[:,2],'-',label ='$\omega^3$',linewidth = 2)
+        plt.legend()
+        for v in vmax:
+            plt.plot([0, self.duration],[v, v], '-.',color = 'k')
+        for v in vmax:
+            plt.plot([0, self.duration],[-v, -v], '-.',color = 'k')
+        ylabel('Angular velocities (rad/s)')
+        xlabel('Time (s)')
+
+        alphavect = array([self.EvalAlpha(t) for t in tvect])
+        figure(figstart+1)
+        clf()
+        plt.plot(tvect,alphavect[:,0],'--',label = '$\dot \omega^1$',linewidth = 2)
+        plt.plot(tvect,alphavect[:,1],'-.',label = '$\dot \omega^2$',linewidth = 2)
+        plt.plot(tvect,alphavect[:,2],'-',label = '$\dot \omega^3$',linewidth = 2)      
+        plt.legend()
+        for a in accelmax:
+            plt.plot([0, self.duration],[a, a], '-.',color = 'k')
+        for a in accelmax:
+            plt.plot([0, self.duration],[-a, -a], '-.',color = 'k')
+        ylabel('Angular accelerations (rad/s^2)')
+        xlabel('Time (s)')
+
+        if not(I is None):
+            torquesvect = array([self.EvalTorques(t,I) for t in tvect])
+            figure(figstart+2)
+            clf()
+            plt.plot(tvect,torquesvect[:,0],'--',label = r'$\tau^1$',linewidth = 2)
+            plt.plot(tvect,torquesvect[:,1],'-.',label = r'$\tau^2$',linewidth = 2)
+            plt.plot(tvect,torquesvect[:,2],'-',label = r'$\tau^3$',linewidth = 2)
+            plt.legend()
+            
+            for tau in taumax:
+                plt.plot([0, self.duration],[tau, tau], '-.',color = 'k')
+            for tau in taumax:
+                plt.plot([0, self.duration],[-tau, -tau], '-.',color = 'k')
