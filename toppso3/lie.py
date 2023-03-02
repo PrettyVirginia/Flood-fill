0ed11c869c471dae4302bd60fb8cd366b64fc79f
PrettyVirginia <116265285+PrettyVirginia@users.noreply.github.com>
2023-03-02 11:56:45 +0000

    Remove unnecessary file.

--- toppso3/lie.py
+++ toppso3/lie.py
@@ -14,3 +14,49 @@ import matplotlib.pyplot as plt
 
 
 Eps = zeros((3,3,3))
+Eps[0,2,1] = -1
+Eps[0,1,2] = 1
+Eps[1,0,2] = -1
+Eps[1,2,0] = 1
+Eps[2,1,0] = -1
+Eps[2,0,1] = 1
+
+
+class LieTraj():
+    def __init__(self, Rlist,trajlist):
+        self.Rlist = Rlist
+        self.trajlist = trajlist
+        self.duration = 0
+        self.trajcumulateddurationslist = []
+        for t in trajlist:
+            self.trajcumulateddurationslist.append(self.duration)
+            self.duration += t.duration
+
+    def FindTrajIndex(self, s):
+        if s == 0:
+            s = 1e-10
+        i = bisect.bisect_left(self.trajcumulateddurationslist, s) - 1
+        remainder = s - self.trajcumulateddurationslist[i]
+        return i, remainder
+
+    # Rotation
+    def EvalRotation(self,s):
+        i, remainder = self.FindTrajIndex(s)
+        return(dot(self.Rlist[i],expmat(self.trajlist[i].Eval(remainder))))
+
+    # Velocity in body frame
+    def EvalOmega(self,s):
+        i, remainder = self.FindTrajIndex(s)
+        r = self.trajlist[i].Eval(remainder)
+        rd = self.trajlist[i].Evald(remainder)
+        return dot(Amat(r),rd)
+
+    # Acceleration in body frame
+    def EvalAlpha(self,s):
+        i, remainder = self.FindTrajIndex(s)
+        r = self.trajlist[i].Eval(remainder)
+        rd = self.trajlist[i].Evald(remainder)
+        rdd = self.trajlist[i].Evaldd(remainder)
+        return dot(Bmat(r),rdd) + dot(rd,tensordot(Ctensor(r),rd,([2],[0])))
+
+    # Torques
